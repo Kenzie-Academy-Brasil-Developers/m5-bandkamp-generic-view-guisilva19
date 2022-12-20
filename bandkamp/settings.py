71d40978958981312c57e76550156fe168dacbf7
unknown <2003silvagui@gmail.com>
2022-12-19 22:10:15 -0300

    fix: relation psql

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,9 +92,11 @@ WSGI_APPLICATION = "bandkamp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME" : os.getenv('POSTGRES_DB_NAME'),
-        "USER" : os.getenv('POSTGRES_USERNAME'),
-        "PASSWORD": os.getenv('POSTGRES_PASSWORD')
+        "NAME": os.getenv("POSTGRES_DB_NAME"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     },
     "other_name": {
         "ENGINE": "django.db.backends.sqlite3",
